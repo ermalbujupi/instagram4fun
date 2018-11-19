e871f8a00abd9fa83b340e882c8fe5fa2ab519e6
Ermal <ermal.bujupaj@openresearch.com>
2018-11-19 20:56:34 +0100

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ermal Buju on 19.11.18.
+//  Copyright © 2018 Ermal Bujupaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
